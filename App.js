f23490395feff28d6ada81885735503d29374f7a
asadayyubi <asad.ayyubi25@gmail.com>
2022-07-18 18:19:32 +0530

    completed university website initial commit

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
-
+var displayButton = document.getElementById('nav-links')
+function showMenu() {
+    displayButton.style.right = "0";
+    
+}
+function hideMenu() {
+    displayButton.style.right = "-200px";
+    
+}
